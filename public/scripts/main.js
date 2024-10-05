8dfb28b96f2e777eb97e5904575596f4faf5b464
tanpero <camille-dolma@outlook.com>
2024-10-05 15:25:55 +0800

    Add heading animation

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -7,13 +7,22 @@ const typewriter = new Typewriter(heading, {
   delay: 75,
 });
 
+const blue = "#66d9ef"
+const green = "#a6e22e"
+const yellow = "#e6db74"
+const red = "#f92772"
+const s = color => `<span style="color: ${color}">`
+const c = (word, color) => s(color) + word + "</span>"
+
 typewriter
-  .pauseFor(2500)
-  .typeString('A simple yet powerful native javascript')
-  .pauseFor(300)
-  .deleteChars(10)
-  .typeString('<strong>JS</strong> plugin for a cool typewriter effect and ')
-  .typeString('<strong>only <span style="color: #27ae60;">5kb</span> Gzipped!</strong>')
+  .pauseFor(1500)
+  .typeString(`${c("Futamura1", blue)} ${c("::", yellow)} ${c("Program", green)} ${s(red)}-> ${c("Interpreter", green)} ${s(red)}-> ${c("Compiler", green)}`)
+  .pauseFor(500)
+  .deleteChars(39)
+  .typeString(`${c("2", blue)} ${c("::", yellow)} ${c("Interpreter", green)} ${s(red)}-> ${c("Compiler", green)} ${s(red)}-> ${c("Compiler", green)}`)
+  .pauseFor(500)
+  .deleteChars(40)
+  .typeString(`${c("3", blue)} ${c("::", yellow)} ${c("Compiler", green)} ${s(red)}-> ${c("Compiler", green)} ${s(red)}-> ${c("Compiler", green)}`)
   .pauseFor(1000)
   .start();
 
